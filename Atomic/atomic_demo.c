0b414ee9189894fc4e67cbc2cbbc9dee1db7ec19
Stephen Hemminger <stephen@networkplumber.org>
2022-04-13 17:24:13 -0700

    Update atomic demo

--- Atomic/atomic_demo.c
+++ Atomic/atomic_demo.c
@@ -21,6 +21,7 @@ static int demo_atomic_major;
 
 static struct global_data {
 	struct cdev cdev;
+	atomic_t refcnt;
 	atomic_long_t counter;
 	struct delayed_work dwork;
 } global;
@@ -32,8 +33,8 @@ static void counter_tick(struct work_struct *work)
 	schedule_delayed_work(&global.dwork, HZ);
 }
 
-static ssize_t demo_atomic_read(struct file *filp, char __user *buf, size_t count,
-			 loff_t *off)
+static ssize_t
+demo_atomic_read(struct file *filp, char __user *buf, size_t count, loff_t *off)
 {
 	char tmp[64];
 	loff_t pos;
@@ -55,8 +56,8 @@ static ssize_t demo_atomic_read(struct file *filp, char __user *buf, size_t coun
 	return ret;
 }
 
-static ssize_t demo_atomic_write(struct file *filp, const char __user *buf, size_t len,
-			  loff_t *off)
+static ssize_t
+demo_atomic_write(struct file *filp, const char __user *buf, size_t len, loff_t *off)
 {
 	long val;
 	int ret;
@@ -72,16 +73,20 @@ static ssize_t demo_atomic_write(struct file *filp, const char __user *buf, size
 
 static int demo_atomic_open(struct inode *inode, struct file *file)
 {
-	pr_info("%s called\n", __func__);
-	schedule_delayed_work(&global.dwork, HZ);
+	if (atomic_inc_return(&global.refcnt) == 1) {
+		pr_info("starting work\n");
+		schedule_delayed_work(&global.dwork, HZ);
+	}
 
 	return 0;
 }
 
 static int demo_atomic_release(struct inode *inode, struct file *file)
 {
-	pr_info("%s called\n", __func__);
-	cancel_delayed_work(&global.dwork);
+	if (atomic_dec_and_test(&global.refcnt)) {
+		pr_info("stopping timer\n");
+		cancel_delayed_work(&global.dwork);
+	}
 
 	return 0;
 }
@@ -95,13 +100,13 @@ static const struct file_operations fops = {
 	.release = demo_atomic_release,
 };
 
-static int __init demo_atomic_driver_init(void)
+static int __init demo_atomic_init(void)
 {
 	struct device *cdev;
 	int retval;
 	dev_t dev;
 	
-	retval = alloc_chrdev_region(&dev, 0, 1, "demo_atomic_dev");
+	retval = alloc_chrdev_region(&dev, 0, 1, "demo_atomic");
 	if (retval < 0) {
 		pr_err("Cannot allocate major number\n");
 		goto err_region;
@@ -109,6 +114,7 @@ static int __init demo_atomic_driver_init(void)
 	demo_atomic_major = MAJOR(dev);
 
 	atomic_long_set(&global.counter, 0);
+	atomic_set(&global.refcnt, 0);
 
 	INIT_DELAYED_WORK(&global.dwork, counter_tick);
 	cdev_init(&global.cdev, &fops);
@@ -119,21 +125,19 @@ static int __init demo_atomic_driver_init(void)
 		goto err_add;
 	}
 
-	demo_atomic_class = class_create(THIS_MODULE, "atomic_demo");
+	demo_atomic_class = class_create(THIS_MODULE, "demo_atomic");
 	if (IS_ERR(demo_atomic_class)) {
 		retval = PTR_ERR(demo_atomic_class);
-		pr_err("Cannot create the struct class\n");
+		pr_err("cannot create the struct class\n");
 		goto err_class;
 	}
 
 	cdev = device_create(demo_atomic_class, NULL, dev, NULL, "demo_atomic");
 	if (IS_ERR(cdev)) {
 		retval = PTR_ERR(cdev);
-		pr_info("Cannot create the Device\n");
+		pr_err("cannot create the Device\n");
 		goto err_device;
 	}
-
-	schedule_delayed_work(&global.dwork, HZ);
 
 	pr_info("Created demo device %d\n", demo_atomic_major);
 	return 0;
@@ -148,7 +152,7 @@ err_region:
 	return retval;
 }
 
-static void __exit demo_atomic_driver_exit(void)
+static void __exit demo_atomic_exit(void)
 {
 	device_destroy(demo_atomic_class, MKDEV(demo_atomic_major, 0));
 	cdev_del(&global.cdev);
@@ -159,7 +163,7 @@ static void __exit demo_atomic_driver_exit(void)
 	pr_info("Device Driver Remove...Done!!\n");
 }
 
-module_init(demo_atomic_driver_init);
-module_exit(demo_atomic_driver_exit);
+module_init(demo_atomic_init);
+module_exit(demo_atomic_exit);
 
 MODULE_LICENSE("GPL");
