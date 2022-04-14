3f33c1c15731054a6b029a6a74448784d69d649b
Stephen Hemminger <stephen@networkplumber.org>
2022-04-13 17:21:48 -0700

    rwlock: rename

--- RwLock/rwdemo.c
+++ RwLock/rwdemo.c
@@ -22,43 +22,48 @@ static int demo_major;
 static struct global_data {
 	struct cdev cdev;
 	unsigned long counter;
-
 	rwlock_t lock;
-
 	struct delayed_work dwork;
 } global;
 
 /* Called from delayed work every second to increment counter. */
 static void counter_tick(struct work_struct *work)
 {
+	pr_info("delayed work called\n");
+	
 	write_lock(&global.lock);
 	++global.counter;
 	write_unlock(&global.lock);
 
-	schedule_delayed_work(&global.dwork, HZ); /* or msecs_to_jiffies(1000)) */
+	schedule_delayed_work(&global.dwork, HZ);
 }
 
 static ssize_t demo_read(struct file *filp, char __user *buf, size_t count,
 			 loff_t *off)
 {
 	char tmp[64];
+	loff_t pos;
+	ssize_t ret;
 	int len;
 
 	read_lock(&global.lock);
 	len = snprintf(tmp, sizeof(tmp), "%lu\n", global.counter);
 	read_unlock(&global.lock);
 
-	count = min_t(size_t, count, len);
-	
-	/* XXX doesn't do partial read */
-	if (copy_to_user(buf, tmp, count))
+	pos = *off;
+	if (pos >= len)
+		return 0; /* read past eof */
+
+	ret = min_t(ssize_t, count, len - pos);
+	if (copy_to_user(buf, tmp + pos, ret))
 		return -EFAULT;
 
-	return count;
+	*off += ret;
+	return ret;
 }
 
-static ssize_t demo_write(struct file *filp, const char __user *buf, size_t len,
-			  loff_t *off)
+static ssize_t
+demo_write(struct file *filp, const char __user *buf, size_t len, loff_t *off)
 {
 	unsigned long val;
 	int ret;
@@ -69,10 +74,11 @@ static ssize_t demo_write(struct file *filp, const char __user *buf, size_t len,
 	
 	write_lock(&global.lock);
 	global.counter = val;
-	write_lock(&global.lock);
+	write_unlock(&global.lock);
 
 	return len;
 }
+
 
 static const struct file_operations fops = {
 	.owner  = THIS_MODULE,
@@ -80,13 +86,13 @@ static const struct file_operations fops = {
 	.write  = demo_write,
 };
 
-static int __init demo_driver_init(void)
+static int __init demo_rwlock_init(void)
 {
 	struct device *cdev;
 	int retval;
 	dev_t dev;
 	
-	retval = alloc_chrdev_region(&dev, 0, 1, "demo_dev");
+	retval = alloc_chrdev_region(&dev, 0, 1, "demo_rwlock");
 	if (retval < 0) {
 		pr_err("Cannot allocate major number\n");
 		goto err_region;
@@ -103,19 +109,21 @@ static int __init demo_driver_init(void)
 		goto err_add;
 	}
 
-	demo_class = class_create(THIS_MODULE, "counter_demo");
+	demo_class = class_create(THIS_MODULE, "demo_rwlock")
 	if (IS_ERR(demo_class)) {
 		retval = PTR_ERR(demo_class);
 		pr_err("Cannot create the struct class\n");
 		goto err_class;
 	}
 
-	cdev = device_create(demo_class, NULL, dev, NULL, "demo_device");
+	cdev = device_create(demo_class, NULL, dev, NULL, "demo_rwlock");
 	if (IS_ERR(cdev)) {
 		retval = PTR_ERR(cdev);
 		pr_info("Cannot create the Device\n");
 		goto err_device;
 	}
+
+	schedule_delayed_work(&global.dwork, HZ);
 
 	pr_info("Created demo device %d\n", demo_major);
 	return 0;
@@ -130,7 +138,7 @@ err_region:
 	return retval;
 }
 
-static void __exit demo_driver_exit(void)
+static void __exit demo_rwlock_exit(void)
 {
 	device_destroy(demo_class, MKDEV(demo_major, 0));
 	cdev_del(&global.cdev);
@@ -141,7 +149,7 @@ static void __exit demo_driver_exit(void)
 	pr_info("Device Driver Remove...Done!!\n");
 }
 
-module_init(demo_driver_init);
-module_exit(demo_driver_exit);
+module_init(demo_rwlock_init);
+module_exit(demo_rwlock_exit);
 
 MODULE_LICENSE("GPL");
